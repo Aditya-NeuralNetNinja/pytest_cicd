f80011bb2def1632fcd4034be80cdaa7cf841e3a
Aditya-NeuralNetNinja <adityachrs7@gmail.com>
2024-09-28 18:06:34 +0530

    code files added

---
+++ test_cases.py
@@ -0,0 +1,15 @@
+from pytest_cicd.multiply import mult
+
+def test_case1(): 
+    result = 10
+    assert mult(5,2) == result
+    
+
+def test_case2():
+    result = 6
+    assert mult(2,5) == result
+    
+    
+def test_case3():
+    result = 12
+    assert mult(4,3) == result
